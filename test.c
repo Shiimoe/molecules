66845e5a64b7e1f847d82219f04f501fb158a226
Shiimoe <samuelfrost@tuta.io>
2022-08-16 20:42:34 +0100

    fixed colliding issues, added bar chart to visualise Boltzmann curve

--- test.c
+++ test.c
@@ -4,9 +4,10 @@
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
-#define BALLS 1500
+#define BALLS 500
 #define ballRadius 10
-#define SPEED 10
+#define SPEED 15
+#define POPULATION_SIZE SPEED + 10
 
 int main(void)
 {
@@ -22,8 +23,8 @@ int main(void)
 		Velocities[i] = (Vector2){(rand() % SPEED) - (rand() % SPEED), (rand() % SPEED) - (rand() % SPEED)};
 	}
 
-	// Vector2 Positions[BALLS] = {{50, 250}, {400, 250}, {700, 100}};
-	// Vector2 Velocities[BALLS] = {{3, 3}, {0, 0}, {0, 0}};
+	// Vector2 Positions[BALLS] = {{600, 600}, {300, 600}, {900, 600}, {600, 900}, {600, 300}, {900, 900}, {300, 300}, {300, 900}, {900,300}};
+	// Vector2 Velocities[BALLS] = {{10, 0}, {2, 0}, {-2, 0}, {0, -2}, {0, 2}, {-2, -2}, {2, 2}, {2, -2}, {-2, 2}};
 
     SetTargetFPS(60);
 
@@ -37,7 +38,8 @@ int main(void)
 			Vector2 velocity = Velocities[i];
 			Positions[i].x += Velocities[i].x;
 			Positions[i].y += Velocities[i].y;
-
+		}
+		for (int i = 0; i < BALLS; i++) {
 
 			if (Positions[i].x >= screenWidth - ballRadius) {
 				Velocities[i].x = -Velocities[i].x;
@@ -57,37 +59,41 @@ int main(void)
 
 			for (int j=i+1; j < BALLS; j++){
 				if (sqrtf(powf(Positions[i].x - Positions[j].x, 2) + powf(Positions[i].y - Positions[j].y, 2)) <= 2 * ballRadius) {
+					// moving molecules so they don't overlap on collision 
+					
+					float ratio = Vector2Length(Velocities[i])/(Vector2Length(Velocities[i]) + Vector2Length(Velocities[j]));
 
+					Vector2 diff_ij = Vector2Subtract(Positions[i], Positions[j]);
+
+					Vector2 dP = Vector2Scale(Vector2Scale(diff_ij, 1 / (Vector2Length(diff_ij))), (2 * ballRadius - Vector2Length(diff_ij)));
+					dP.x = fabs(dP.x);
+					dP.y = fabs(dP.y);
+
+					if (Positions[i].x  > Positions[j].x) {
+						Positions[i].x += dP.x * ratio;
+						Positions[j].x -= dP.x * (1 - ratio);
+					}
+					else if (Positions[i].x < Positions[j].x) {
+						 Positions[i].x -= dP.x * ratio;
+						 Positions[j].x +=  dP.x * (1 - ratio);
+					}
+
+					if (Positions[i].y > Positions[j].y) {
+						 Positions[i].y += dP.y * ratio;
+						 Positions[j].y -= dP.y * (1 - ratio);
+					}
+					else if (Positions[i].y < Positions[j].y) {
+						 Positions[i].y -= dP.y * ratio;
+						 Positions[j].y += dP.y * (1 - ratio);
+					}
+					
+					
+					// calculating new velocity
 					float dot = Vector2DotProduct(Vector2Subtract(Velocities[i], Velocities[j]), Vector2Subtract(Positions[i], Positions[j]));
 					float normSquared = Vector2LengthSqr(Vector2Subtract(Positions[i], Positions[j]));
 
 					Velocities[i] = Vector2Subtract(Velocities[i], Vector2Scale(Vector2Subtract(Positions[i], Positions[j]), dot/normSquared));
 					Velocities[j] = Vector2Subtract(Velocities[j], Vector2Scale(Vector2Subtract(Positions[i], Positions[j]), -dot/normSquared));
-
-
-					Vector2 dP = Vector2Scale(Vector2Scale(Vector2Subtract(Positions[i], Positions[j]), 
-					1 / (Vector2Length(Vector2Subtract(Positions[i], Positions[j])))), 
-					(2 * ballRadius - Vector2Length(Vector2Subtract(Positions[i], Positions[j]))));
-					dP.x = fabs(dP.x);
-					dP.y = fabs(dP.y);
-
-					if (Positions[i].x  > Positions[j].x) {
-						Positions[i].x += dP.x/2;
-						Positions[j].x -= dP.x/2;
-					}
-					else if (Positions[i].x < Positions[j].x) {
-						 Positions[i].x -= dP.x/2;
-						 Positions[j].x +=  dP.x/2;
-					}
-
-					if (Positions[i].y > Positions[j].y) {
-						 Positions[i].y += dP.y/2;
-						 Positions[j].y -= dP.y/2;
-					}
-					else if (Positions[i].y < Positions[j].y) {
-						 Positions[i].y -= dP.y/2;
-						 Positions[j].y += dP.y/2;
-					}
 				}
 			}
 
@@ -96,6 +102,7 @@ int main(void)
 		BeginDrawing(); // I wish this were wrapped in curly bwackets qwq
 
             ClearBackground(RAYWHITE);
+			int population[POPULATION_SIZE];
 
 			// DrawCircleV(ballPosition, ballRadius * 2, RED);
 			float momentum = 0;
@@ -106,9 +113,19 @@ int main(void)
 				else DrawCircleV(Positions[i], ballRadius, RED);
 				tot_x += powf(Velocities[i].x, 2);
 				tot_y += powf(Velocities[i].y, 2);
+				for (int j = 0; j < POPULATION_SIZE; j += 1) {
+					if (j < sqrtf(Vector2LengthSqr(Velocities[i])) && sqrtf(Vector2LengthSqr(Velocities[i])) < j + 1) { 
+						population[(int)j/1] += 1;
+					}
+				}
+			}
+			
+			for (int i = 0; i < POPULATION_SIZE; i++) {
+				DrawLineEx((Vector2){10 + (i*10), screenHeight-50}, (Vector2){10 + (i*10), (screenHeight - 60 - (population[i] * 10 / log(BALLS)))}, 5, BLACK);
+				population[i] = 0;
 			}
 			momentum = sqrtf(tot_x + tot_y);
-			DrawText(TextFormat("%.10f", momentum), 20, 20, 100, BLACK);
+			DrawText(TextFormat("%.3f", momentum), 20, 20, 100, GRAY);
 
 
 		EndDrawing();
